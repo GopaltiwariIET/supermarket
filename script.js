502155df1bd3031d23d98c8420f3067d8aa23a44
Gopal Tiwari <98863292+GopaltiwariIET@users.noreply.github.com>
2023-10-01 15:05:36 +0530

    done_3

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 let btn = document.getElementById("login_btn");
         btn.addEventListener("click", () => {
-            window.location = '/form';
+            window.location = '/supermarket/form';
         })
